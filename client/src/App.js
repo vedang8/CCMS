343bccb2b98dabfed67d39300a069352c58e0ddb
vedang8 <vedang2607@gmail.com>
2023-12-11 00:24:59 +0530

    created front page

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import {Button} from 'antd';
 
 function App() {
   return (
-    <div className="bg-primary h-screen">
-      <div className="bg-white">
+    <div className="bg-primary h-screen flex justify-center items-center">
+      <div className="bg-white p-5">
         <h1>Welcome to Carbon Credit System</h1>
         <Button type='primary'>
            Antd
