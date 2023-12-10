b6e4b74e1c0b9f4f77a05eb93547ac167dd7f00f
vedang8 <vedang2607@gmail.com>
2023-12-10 16:44:46 +0530

    created the front end

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import {Button} from 'antd';
+
+function App() {
+  return (
+    <div className="bg-primary h-screen">
+      <div className="bg-white">
+        <h1>Welcome to Carbon Credit System</h1>
+        <Button type='primary'>
+           Antd
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
