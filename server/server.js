caf979c65d693b7520dd29a3e627a62a49a6e3df
vedang8 <vedang2607@gmail.com>
2024-01-01 11:04:08 +0530

    made a server

---
+++ server/server.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+app.listen(port, ()=>console.log(` Node js server started on Listening on port ${port})`));
