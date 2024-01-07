d084da1b6b127e2926d7a4356d7533b01938e6b6
vedang8 <vedang2607@gmail.com>
2024-01-07 12:00:15 +0530

    user model created

---
+++ server/routes/usersRoute.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const User = require('../models/userModel');
+
+// new user registration
+router.post('/register', async(req, res) =>{
+
+});
--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const dbconfig = require('./config/dbConfig');
 const port = process.env.PORT || 5000;
 app.listen(port, ()=>console.log(` Node js server started on Listening on port ${port})`));
